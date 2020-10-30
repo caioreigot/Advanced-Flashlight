1477213684e68ca59ca02c83893c7081e581db39
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-10-30 02:03:26 -0300

    Initial commit

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+
+    public Transform characterBody;
+    public Transform characterHead;
+
+    ushort sensitivityX = 100;
+    ushort sensitivityY = 100;
+
+    float rotationX;
+    float rotationY;
+
+    float angleYMin = -90;
+    float angleYMax = 90;
+
+    public bool isLocked = false;
+
+    // float smoothRotx = 0;
+    // float smoothRoty = 0;
+
+    // float smoothCoefx = 0.005f;
+    // float smoothCoefy = 0.005f;
+
+    void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate() {
+        transform.position = characterHead.position;
+    }
+
+    void Update() {
+        if (!isLocked) {
+            float horizontalDelta = Input.GetAxisRaw("Mouse X");
+            float verticalDelta = Input.GetAxisRaw("Mouse Y");
+
+            /*
+            // Smooth Camera
+            smoothRotx = Mathf.Lerp(smoothRotx, horizontalDelta, smoothCoefx);
+            smoothRoty = Mathf.Lerp(smoothRoty, verticalDelta, smoothCoefy);
+            */
+
+            rotationX += horizontalDelta * Time.deltaTime * sensitivityX;
+            rotationY += verticalDelta * Time.deltaTime * sensitivityY;
+
+            rotationY = Mathf.Clamp(rotationY, angleYMin, angleYMax);
+
+            characterBody.localEulerAngles = new Vector3(0, rotationX, 0);
+
+            transform.eulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public GameObject lightSource;
+    public Transform defaultRotation;
+    public AudioSource soundOn;
+    public AudioSource soundOff;
+
+    private bool isOn = false;
+    private bool rotating = false;
+
+    private int rotationSpeed = 35;
+    
+    void Update() {
+        HandleInput();
+        HandleRotate();
+    }
+
+    void HandleInput() {
+        if (Input.GetButtonDown("Fkey")) {
+            if (!isOn)
+                FlashlightOn();
+            else 
+                FlashlightOff();
+        }
+    }
+
+    void FlashlightOn() {
+        lightSource.SetActive(true);
+        soundOn.Play();
+        isOn = true;
+    }  
+
+    void FlashlightOff() {
+        lightSource.SetActive(false);
+        soundOff.Play();
+        isOn = false;
+    }
+
+    void HandleRotate() {
+        if (Input.GetMouseButton(1)) {
+            rotating = true;
+            FindObjectOfType<FirstPersonCamera>().isLocked = true;
+
+            float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
+            float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
+
+            transform.Rotate(Vector3.left, -rotX);
+            transform.Rotate(Vector3.forward, rotY);
+        }
+        if (Input.GetMouseButtonUp(1)) {
+            rotating = false;
+            FindObjectOfType<FirstPersonCamera>().isLocked = false;
+            StartCoroutine(ResetRotation());
+        }
+    }
+
+    IEnumerator ResetRotation() {
+        while (true) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, defaultRotation.transform.rotation, 0.1f);
+            
+            if (transform.rotation == defaultRotation.transform.rotation || rotating)
+                yield break;
+            
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+}
