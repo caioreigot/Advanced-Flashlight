25d4febd745eec7d6000be8d1ad7641ffe393751
Caio Costa <caiocrrodrigues2004@gmail.com>
2021-03-05 08:34:13 -0300

    Added field of view slider to pause menu

---
+++ Assets/Scripts/FOVSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FOVSlider : MonoBehaviour {
+
+    Camera playerCamera;
+    public Text fovText;
+
+    [HideInInspector]
+    public Slider slider;
+
+    void Awake() {
+        slider = GetComponent<Slider>();
+        playerCamera = Camera.main;
+        OnValueChanged();
+    }
+
+    void Update() {
+        fovText.text = slider.value.ToString("0");
+    }
+
+    public void OnValueChanged() {
+        playerCamera.fieldOfView = (int)slider.value;
+    }
+
+}
